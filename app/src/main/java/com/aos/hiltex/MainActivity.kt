ba1ddcfc7cd1e3470326a3048e5f50f555c8fa85
HYUNJUNE PARK <june2ac4dev@gmail.com>
2024-01-13 15:29:10 +0900

    add dataBinding

--- app/src/main/java/com/aos/hiltex/MainActivity.kt
+++ app/src/main/java/com/aos/hiltex/MainActivity.kt
@@ -2,10 +2,19 @@ package com.aos.hiltex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.aos.hiltex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this@MainActivity
+    }
+
+    fun onTestButtonClicked() {
+        Toast.makeText(this, "TestButton", Toast.LENGTH_SHORT).show()
     }
 }
